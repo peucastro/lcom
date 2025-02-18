adeab3a67870a08598dd31ec76dfc76e79952c22
Pedro Castro <falecompedroac@gmail.com>
2025-02-18 22:36:00 +0000

    lab0/ex2: corrected comments

--- lab0/ex2.c
+++ lab0/ex2.c
@@ -10,19 +10,19 @@ void print_array(int *p, int n) {
 
 int main() {
   // pointers
-  // & -> get memory address of a variable
-  // * -> get value stored at a memory address
+  // & -> get memory address of a variable (address-of operator)
+  // * -> get value stored at a memory address (dereference operator)
   int n = 5;
   int *p = &n;
   printf("n=%d\n", n); // %d - format to a decimal
   printf("*p=%d\n", *p);
 
   // print the memory position of p:
-  printf("p=%u\n", p); // %u - format to an unsigned int
+  printf("p=%p\n", (void *) p); // %p - format to a pointer
 
   // pointers of pointers -> variables which value is the memory location of a pointer
   int **pp = &p;
-  printf("*pp=%u\n", *pp); // equivalent to the last print because p and *pp both hold the same address
+  printf("*pp=%p\n", (void *) *pp); // equivalent to the last print because p and *pp both hold the same address
 
   // pointer casting
   *p = 0;
@@ -33,7 +33,7 @@ int main() {
 
   // disclaimer: this will output 4, as minix is a 32 bit machine.
   // if you compile this and run it at your host machine, it will output 8
-  printf("sizeof(int) = %lu\n", sizeof(int));
+  printf("sizeof(int) = %zu\n", sizeof(int));
 
   // arrays
   int s[100] = {0};                   // all zeroes by the compiler
@@ -48,8 +48,8 @@ int main() {
   printf("*(s+1)=%d\n", *(s + 1)); // 1 (equivalent to the last print)
                                    // pointer arithmetic -> automatically adjusts the offset depending on the base type)
 
-  printf("sizeof(s)=%u\n", sizeof(s)); // outputs 400, allocated at compile time
-  printf("sizeof(d)=%u\n", sizeof(d)); // outputs 4, the size of the pointer
+  printf("sizeof(s)=%zu\n", sizeof(s)); // outputs 400, allocated at compile time
+  printf("sizeof(d)=%zu\n", sizeof(d)); // outputs the size of the pointer
 
   print_array(s, 5); // s and d are both pointers to the first position
   print_array(d, 5); // of their respective array
