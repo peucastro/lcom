4b9fa28b910e85dfdede198b6c6096915f67056a
Pedro Castro <falecompedroac@gmail.com>
2025-02-25 09:44:56 +0000

    lab2: initial timer_get_conf

--- lab2/timer.c
+++ lab2/timer.c
@@ -5,40 +5,49 @@
 
 #include "i8254.h"
 
-int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
+int(timer_set_frequency)(uint8_t timer, uint32_t freq) {
   /* To be implemented by the students */
   printf("%s is not yet implemented!\n", __func__);
 
   return 1;
 }
 
-int (timer_subscribe_int)(uint8_t *bit_no) {
-    /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
-}
-
-int (timer_unsubscribe_int)() {
+int(timer_subscribe_int)(uint8_t *bit_no) {
   /* To be implemented by the students */
   printf("%s is not yet implemented!\n", __func__);
 
   return 1;
 }
 
-void (timer_int_handler)() {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-}
-
-int (timer_get_conf)(uint8_t timer, uint8_t *st) {
+int(timer_unsubscribe_int)() {
   /* To be implemented by the students */
   printf("%s is not yet implemented!\n", __func__);
 
   return 1;
 }
 
-int (timer_display_conf)(uint8_t timer, uint8_t st,
+void(timer_int_handler)() {
+  /* To be implemented by the students */
+  printf("%s is not yet implemented!\n", __func__);
+}
+
+int(timer_get_conf)(uint8_t timer, uint8_t *st) {
+  if (st == NULL)
+    return 1;
+
+  uint8_t cmd = TIMER_RB_CMD | TIMER_RB_COUNT_ | TIMER_RB_SEL(timer);
+  int ret = sys_outb(TIMER_CTRL, cmd);
+  if (ret != 0)
+    return 1;
+
+  ret = util_sys_inb(TIMER_0 + timer, st);
+  if (ret != 0)
+    return 1;
+
+  return 0;
+}
+
+int(timer_display_conf)(uint8_t timer, uint8_t st,
                         enum timer_status_field field) {
   /* To be implemented by the students */
   printf("%s is not yet implemented!\n", __func__);
