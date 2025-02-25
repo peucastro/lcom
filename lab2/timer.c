0da7d7345115e9817eee27004a8959d2be4b3e19
Pedro Castro <falecompedroac@gmail.com>
2025-02-25 11:17:47 +0000

    lab2: implemented timer_display_conf

--- lab2/timer.c
+++ lab2/timer.c
@@ -35,8 +35,8 @@ int(timer_get_conf)(uint8_t timer, uint8_t *st) {
   if (st == NULL)
     return 1;
 
-  uint8_t cmd = TIMER_RB_CMD | TIMER_RB_COUNT_ | TIMER_RB_SEL(timer);
-  int ret = sys_outb(TIMER_CTRL, cmd);
+  uint8_t rdb_cmd = TIMER_RB_CMD | TIMER_RB_COUNT_ | TIMER_RB_SEL(timer);
+  int ret = sys_outb(TIMER_CTRL, rdb_cmd);
   if (ret != 0)
     return 1;
 
@@ -49,8 +49,57 @@ int(timer_get_conf)(uint8_t timer, uint8_t *st) {
 
 int(timer_display_conf)(uint8_t timer, uint8_t st,
                         enum timer_status_field field) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  union timer_status_field_val val;
 
-  return 1;
+  switch (field) {
+    case tsf_all:
+      val.byte = st;
+      break;
+
+    case tsf_initial:
+      st = ((st >> 4) & 0x3);
+
+      if (st == 1) {
+        val.in_mode = LSB_only;
+      }
+      else if (st == 2) {
+        val.in_mode = MSB_only;
+      }
+      else if (st == 3) {
+        val.in_mode = MSB_after_LSB;
+      }
+      else {
+        return 1;
+      }
+
+      break;
+
+    case tsf_mode:
+      st = ((st >> 1) & 0x7);
+
+      if (st == 6) {
+        val.count_mode = 2;
+      }
+      else if (st == 7) {
+        val.count_mode = 3;
+      }
+      else {
+        val.count_mode = st;
+      }
+
+      break;
+
+    case tsf_base:
+      val.bcd = (st & TIMER_BCD);
+      break;
+
+    default:
+      return 1;
+  }
+
+  int ret = timer_print_config(timer, field, val);
+  if (ret != 0)
+    return 1;
+
+  return 0;
 }
