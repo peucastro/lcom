0f19fb9daf5537f43a51254c131edeabfbcd2e65
Pedro Castro <falecompedroac@gmail.com>
2025-03-18 09:47:15 +0000

    lab3: finished kbd_test_timed_scan

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -12,6 +12,7 @@
 extern uint8_t scancode;
 uint8_t bytes[2];
 extern int cnt_sys_inb;
+extern int counter;
 
 int main(int argc, char *argv[]) {
   // sets the language of LCF messages (can be either EN-US or PT-PT)
@@ -117,8 +118,66 @@ int(kbd_test_poll)() {
 }
 
 int(kbd_test_timed_scan)(uint8_t n) {
-  /* To be completed by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  int ipc_status, r, size = 0;
+  uint8_t bit_no, irq_set_timer, irq_set_kbd, time = 0;
+  message msg;
 
-  return 1;
+  if (timer_subscribe_int(&bit_no) != 0)
+    return 1;
+  irq_set_timer = BIT(bit_no);
+
+  if (kbd_subscribe_int(&bit_no) != 0)
+    return 1;
+  irq_set_kbd = BIT(bit_no);
+
+  while (scancode != BREAK_ESC && time < n) { /* breakcode ESC and timeout check */
+    /* get a request message. */
+    if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0) {
+      printf("driver_receive failed with: %d", r);
+      continue;
+    }
+    if (is_ipc_notify(ipc_status)) { /* received notification */
+      switch (_ENDPOINT_P(msg.m_source)) {
+        case HARDWARE:                             /* hardware interrupt notification */
+          if (msg.m_notify.interrupts & irq_set_kbd) { /* subscribed interrupt */
+            kbc_ih();
+            if (scancode == CODE_HEADER) {
+              bytes[0] = scancode;
+              size++;
+            }
+            else {
+              kbc_ih();
+              bytes[size] = scancode;
+            }
+
+            counter = 0;
+            time = 0;
+            kbd_print_scancode(!(scancode & MAKE_CODE), size + 1, bytes);
+          }
+          if (msg.m_notify.interrupts & irq_set_timer) {
+            timer_int_handler();
+            if (counter % 60 == 0) {
+              time++;
+            }
+          }
+            break;
+        default:
+          break; /* no other notifications expected: do nothing */
+      }
+    }
+    else { /* received a standard message, not a notification */
+      /* no standard messages expected: do nothing */
+    }
+    size = 0;
+  }
+
+  if (kbd_unsubscribe_int() != 0)
+    return 1;
+  if(timer_unsubscribe_int() != 0)
+    return 1;
+
+  if (kbd_print_no_sysinb(cnt_sys_inb) != 0)
+    return 1;
+
+  return 0;
 }
