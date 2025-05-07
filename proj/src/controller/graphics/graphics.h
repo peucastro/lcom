bc7295bce5067fbc347e17c45b60f4ebf2eb7ae7
Pedro Castro <falecompedroac@gmail.com>
2025-05-07 12:39:32 +0100

    uploaded graphics files

---
+++ proj/src/controller/graphics/graphics.h
@@ -0,0 +1,101 @@
+#ifndef __PROJ_GRAPHICS_H
+#define __PROJ_GRAPHICS_H
+
+#include <stdint.h>
+
+#include "controller/graphics/vbe.h"
+
+/** @defgroup graphics graphics
+ * @{
+ *
+ * Functions for using the PC's video card in Graphics Mode.
+ */
+
+/**
+ * @brief Extracts the Red component from a 32 bit color.
+ *
+ * @param color a 32 bit color
+ * @return The Red component of the color
+ */
+uint32_t(extract_red)(uint32_t color);
+
+/**
+ * @brief Extracts the Green component from a 32 bit color.
+ *
+ * @param color a 32 bit color
+ * @return The Green component of the color
+ */
+uint32_t(extract_green)(uint32_t color);
+
+/**
+ * @brief Extracts the Blue component from a 32 bit color.
+ *
+ * @param color a 32 bit color
+ * @return The Blue component of the color
+ */
+uint32_t(extract_blue)(uint32_t color);
+
+/**
+ * @brief Draws a pixel in the screen.
+ *
+ * @param x Horizontal axis coordinate
+ * @param y Vertial axis coordinate
+ * @param color Color to set the pixel
+ * @return Return 0 upon success, non-zero otherwise
+ */
+int(graphics_draw_pixel)(uint16_t x, uint16_t y, uint32_t color);
+
+/**
+ * @brief Draws a horizontal line on the screen.
+ *
+ * @param x Horizontal axis coordinate of the initial point
+ * @param y Vertical axis coordinate of the initial point
+ * @param len Line's length in pixels
+ * @param color Color to draw the line's pixels
+ * @return Return 0 upon success, non-zero otherwise
+ */
+int(graphics_draw_hline)(uint16_t x, uint16_t y, uint16_t len, uint32_t color);
+
+/**
+ * @brief Draws a filled rectangle on the screen.
+ *
+ * @param x Horizontal axis coordinate of the initial point
+ * @param y Vertical axis coordinate of the initial point
+ * @param width Rectangle's width in pixels
+ * @param height Rectangle's height in pixels
+ * @param color Color to set the rectangle's pixels
+ * @return Return 0 upon success, non-zero otherwise
+ */
+int(graphics_draw_rectangle)(uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint32_t color);
+
+/**
+ * @brief Draws a matrix of colored rectangles on the screen.
+ *
+ * @param mode Video mode to set
+ * @param no_rectangles Number of rectangles per row and column
+ * @param first Color of the first rectangle
+ * @param step Step value to calculate the color of subsequent rectangles
+ * @return Return 0 upon success, non-zero otherwise
+ */
+int(graphics_draw_matrix)(uint16_t mode, uint8_t no_rectangles, uint32_t first, uint8_t step);
+
+/**
+ * @brief Draws a pixmap (XPM image) on the screen at the specified coordinates.
+ *
+ * @param xpm Pixmap to be drawn
+ * @param x Horizontal axis coordinate of the upper left corner of the pixmap
+ * @param y Vertical axis coordinate of the upper left corner of the pixmap
+ * @return Return 0 upon success, non-zero otherwise
+ */
+int(graphics_draw_xpm)(xpm_map_t xpm, uint16_t x, uint16_t y);
+
+/**
+ * @brief Clears the entire screen.
+ *
+ * @return Return 0 upon success, non-zero otherwise
+ */
+int(graphics_clear_screen)(void);
+
+/**@}*/
+
+#endif /* __PROJ_GRAPHICS_H */
---
+++ proj/src/controller/graphics/vbe.c
@@ -0,0 +1,164 @@
+#include <lcom/lcf.h>
+
+#include "controller/graphics/vbe.h"
+
+/* static global variable to store vbe mode information.
+ * this structure holds details about the selected video mode */
+static vbe_mode_info_t mode_info;
+/* static global pointer to the mapped video memory in the process's address space.
+ * in minix 3, vram is not directly accessible and needs to be mapped */
+static uint8_t *video_mem;
+
+vbe_mode_info_t(vbe_get_mode)(void) {
+  return mode_info;
+}
+
+uint8_t *(vbe_get_video_mem) (void) {
+  return video_mem;
+}
+
+int(vbe_get_mode_information)(uint16_t mode, vbe_mode_info_t *vmi) {
+  if (vmi == NULL) {
+    fprintf(stderr, "vbe_get_mode_information: vmi pointer cannot be null.");
+    return 1;
+  }
+
+  mmap_t map;
+  if (memset(&map, 0, sizeof(map)) == NULL) {
+    perror("vbe_get_mode_information: failed to clear mmap_t.");
+    return 1;
+  }
+  if (lm_alloc(sizeof(vbe_mode_info_t), &map) == NULL) {
+    perror("vbe_get_mode_information: failed to allocate memory.");
+    return 1;
+  }
+
+  struct reg86 args;
+  if (memset(&args, 0, sizeof(args)) == NULL) {
+    perror("vbe_get_mode_information: failed to clear reg86.");
+    lm_free(&map);
+    return 1;
+  }
+
+  args.ah = VBE_FUNCTION;      // indicate a vbe function call
+  args.al = VBE_RET_MODE;      // function 01h - return VBE mode information
+  args.cx = mode;              // mode number
+  args.es = PB2BASE(map.phys); // segment base address
+  args.di = PB2OFF(map.phys);  // offset within segment
+  args.intno = VBE_INT;        // video BIOS interrupt
+
+  // call sys_int86 to invoke the bios interrupt with the specified register values
+  if (sys_int86(&args) != 0) {
+    perror("vbe_get_mode_information: failed to call sys_int86.");
+    lm_free(&map);
+    return 1;
+  }
+
+  if (args.ax != VBE_FUNCTION) { // AH should be 0x4F if successful
+    fprintf(stderr, "vbe_get_mode_information: BIOS call failed");
+    lm_free(&map);
+    return 1;
+  }
+
+  *vmi = *(vbe_mode_info_t *) map.virt;
+
+  if (lm_free(&map) != true) {
+    perror("vbe_get_mode_information: failed to free map mem.");
+    return 1;
+  }
+  return 0;
+}
+
+int(vbe_set_video_mode)(uint16_t mode) {
+  // holds the arguments for the bios interrupt call
+  struct reg86 args;
+  // clear the reg86 structure to avoid unexpected behavior
+  if (memset(&args, 0, sizeof(args)) == NULL) {
+    perror("vbe_set_video_mode: failed to clear reg86.");
+    return 1;
+  }
+
+  // set ah register to VBE_FUNCTION (0x4f) to indicate a vbe function call
+  args.ah = VBE_FUNCTION;
+  // set al register to VBE_SET_MODE (0x02) to select the "set vbe mode" function
+  args.al = VBE_SET_MODE;
+  /* set bx register to the desired video mode number ored with vbe_linear_mode.
+   * setting bit 14 of bx enables the linear frame buffer model for easier vram access */
+  args.bx = mode | VBE_LINEAR_MODE;
+  // set intno to VBE_INT (0x10) to specify the bios video services interrupt
+  args.intno = VBE_INT;
+
+  // call sys_int86 to invoke the bios interrupt with the specified register values
+  if (sys_int86(&args) != 0) {
+    perror("vbe_set_video_mode: failed to call sys_int86.");
+    return 1;
+  }
+
+  /* check the return status in ah and al registers after the vbe call.
+   * al should be 0x4f if the vbe function is supported, and ah should be 0x00 for success */
+  if (args.ah != VBE_CALL_SUCCESS || args.al != VBE_FUNCTION) {
+    // check if ah indicates a failed function call (0x01).
+    if (args.ah == VBE_CALL_FAIL) {
+      fprintf(stderr, "vbe_set_video_mode: vbe call fail.");
+    }
+    // check if ah indicates the function is not supported in the current hardware configuration (0x02)
+    else if (args.ah == VBE_CALL_NOT_SUPPORTED) {
+      fprintf(stderr, "vbe_set_video_mode: vbe call not supported.");
+    }
+    // check if ah indicates the function is invalid in the current video mode (0x03)
+    else if (args.ah == VBE_CALL_INVALID) {
+      fprintf(stderr, "vbe_set_video_mode: vbe call invalid.");
+    }
+    // handle other non-success return codes in ah
+    else {
+      fprintf(stderr, "vbe_set_video_mode: function not supported.");
+    }
+    return 1;
+  }
+
+  return 0;
+}
+
+int(vbe_map_vram)(uint16_t mode) {
+  /* check if setting the memory of the mode_info structure to zero fails.
+   * mode_info will store the video mode information */
+  if (memset(&mode_info, 0, sizeof(mode_info)) == NULL) {
+    perror("vbe_map_vram: failed to clear mode_info.");
+    return 1;
+  }
+  /* call the vbe_get_mode_info function to retrieve information about the specified video mode.
+   * this function is provided by the lcf */
+  if (vbe_get_mode_information(mode, &mode_info) != 0) {
+    fprintf(stderr, "vbe_map_vram: failed to get mode info.");
+    return 1;
+  }
+
+  // declare a struct minix_mem_range to define the physical memory range
+  struct minix_mem_range mr;
+  // check if setting the memory of the minix_mem_range structure to zero fails
+  if (memset(&mr, 0, sizeof(mr)) == NULL) {
+    perror("vbe_map_vram: failed to clear minix_mem_range.");
+    return 1;
+  }
+  // set the base physical address of the memory range to the physical base pointer obtained from mode_info.
+  mr.mr_base = (phys_bytes) mode_info.PhysBasePtr;
+  // set the limit of the memory range to the base address plus the total vram size.
+  mr.mr_limit = mr.mr_base + mode_info.XResolution * mode_info.YResolution * (mode_info.BitsPerPixel + 7) / 8;
+
+  // request permission to map the specified physical memory range into the process's address space using the sys_privctl kernel call
+  if (sys_privctl(SELF, SYS_PRIV_ADD_MEM, &mr) != 0) {
+    perror("vbe_map_vram: failed to request permission.");
+    return 1;
+  }
+
+  // map the physical memory region into the process's virtual address space using the vm_map_phys kernel call.
+  video_mem = (uint8_t *) vm_map_phys(SELF, (void *) mr.mr_base, mode_info.XResolution * mode_info.YResolution * (mode_info.BitsPerPixel + 7) / 8);
+
+  // check if the memory mapping failed.
+  if ((void *) video_mem == MAP_FAILED) {
+    fprintf(stderr, "vbe_map_vram: map failed.");
+    return 1;
+  }
+
+  return 0;
+}
