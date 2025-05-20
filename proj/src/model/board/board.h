b5d8a671a9b6d57ac2551b712cd5b991e550010d
Pedro Castro <falecompedroac@gmail.com>
2025-05-20 17:25:28 +0100

    removed mallocs from board

--- proj/src/model/board/board.h
+++ proj/src/model/board/board.h
@@ -13,7 +13,9 @@
  * Functions for parsing game board from text files
  */
 
-#define MAX_LINE_LENGTH 64 /**< @brief Maximum line length for reading board files */
+#define MAX_LINE_LENGTH 64  /**< @brief Maximum line length for reading board files */
+#define MAX_BOARD_WIDTH 16  /**< @brief Maximum width of the game board */
+#define MAX_BOARD_HEIGHT 11 /**< @brief Maximum height of the game board */
 
 /**
  * @brief Enum representing different types of board elements
@@ -32,27 +34,29 @@ typedef enum {
  * @brief Struct representing the game board
  */
 typedef struct {
-  int width;               /**< @brief Width of the board in cells */
-  int height;              /**< @brief Height of the board in cells */
-  board_element_t **elements; /**< @brief 2D array of board elements */
+  uint8_t width;                                               /**< @brief Width of the board in cells */
+  uint8_t height;                                              /**< @brief Height of the board in cells */
+  board_element_t elements[MAX_BOARD_HEIGHT][MAX_BOARD_WIDTH]; /**< @brief 2D array of board elements */
 } GameBoard;
 
 /**
  * @brief Creates a new game board from a text file
  *
+ * @param board Pointer to the GameBoard structure to fill
  * @param filename Path to the text file containing the board layout
- * @return Pointer to the created game board, or NULL if an error occurs
- */
-GameBoard *(create_board) (const char *filename);
-
-/**
- * @brief Destroys a game board and frees its resources
- *
- * @param board Pointer to the board to be destroyed
  *
  * @return 0 upon success, non-zero otherwise
  */
-int(destroy_board)(GameBoard *board);
+int(load_board)(GameBoard *board, const char *filename);
+
+/**
+ * @brief Resets a game board
+ *
+ * @param board Pointer to the board to be reset
+ *
+ * @return 0 upon success, non-zero otherwise
+ */
+int(reset_board)(GameBoard *board);
 
 /**@}*/
 
