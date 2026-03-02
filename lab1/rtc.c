5c0bb4648e5dd176e977c8f4cdf1a41f82d68be0
Pedro Castro <falecompedroac@gmail.com>
2026-03-02 20:03:29 +0000

    improved error messages

--- lab1/rtc.c
+++ lab1/rtc.c
@@ -15,31 +15,47 @@ static int bcd_to_bin(uint8_t bcd) {
 }
 
 int rtc_read_date(rtc_date *date) {
-  if (!date)
+  if (date == NULL) {
+    fprintf(stderr, "rtc_read_date: date pointer cannot be null.\n");
     return 1;
+  }
 
   uint32_t reg_a, reg_b, day, month, year;
 
   do {
-    if (sys_outb(RTC_ADDR_REG, RTC_REG_A) != 0)
+    if (sys_outb(RTC_ADDR_REG, RTC_REG_A) != 0) {
+      perror("rtc_read_date: failed to write to RTC address register (Reg A)");
       return 1;
-    if (sys_inb(RTC_DATA_REG, &reg_a) != 0)
+    }
+    if (sys_inb(RTC_DATA_REG, &reg_a) != 0) {
+      perror("rtc_read_date: failed to read from RTC data register (Reg A)");
       return 1;
+    }
   } while (reg_a & RTC_UIP_MSK);
 
-  if (sys_outb(RTC_ADDR_REG, RTC_REG_B) != 0)
+  if (sys_outb(RTC_ADDR_REG, RTC_REG_B) != 0) {
+    perror("rtc_read_date: failed to write to RTC address register (Reg B)");
     return 1;
-  if (sys_inb(RTC_DATA_REG, &reg_b) != 0)
+  }
+  if (sys_inb(RTC_DATA_REG, &reg_b) != 0) {
+    perror("rtc_read_date: failed to read from RTC data register (Reg B)");
     return 1;
+  }
 
   int is_bcd = !(reg_b & RTC_DM_MSK);
 
-  if (sys_outb(RTC_ADDR_REG, RTC_REG_DAY) != 0 || sys_inb(RTC_DATA_REG, &day) != 0)
+  if (sys_outb(RTC_ADDR_REG, RTC_REG_DAY) != 0 || sys_inb(RTC_DATA_REG, &day) != 0) {
+    fprintf(stderr, "rtc_read_date: failed to read Day register.\n");
     return 1;
-  if (sys_outb(RTC_ADDR_REG, RTC_REG_MONTH) != 0 || sys_inb(RTC_DATA_REG, &month) != 0)
+  }
+  if (sys_outb(RTC_ADDR_REG, RTC_REG_MONTH) != 0 || sys_inb(RTC_DATA_REG, &month) != 0) {
+    fprintf(stderr, "rtc_read_date: failed to read Month register.\n");
     return 1;
-  if (sys_outb(RTC_ADDR_REG, RTC_REG_YEAR) != 0 || sys_inb(RTC_DATA_REG, &year) != 0)
+  }
+  if (sys_outb(RTC_ADDR_REG, RTC_REG_YEAR) != 0 || sys_inb(RTC_DATA_REG, &year) != 0) {
+    fprintf(stderr, "rtc_read_date: failed to read Year register.\n");
     return 1;
+  }
 
   date->day = is_bcd ? (uint8_t) bcd_to_bin(day) : (uint8_t) day;
   date->month = is_bcd ? (uint8_t) bcd_to_bin(month) : (uint8_t) month;
